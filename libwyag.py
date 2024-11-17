eb61c0a8be7333cd4fe4f222580aee42f04da36c
mac <mac@macs-MacBook-Air.local>
2024-11-17 13:32:34 +0100

    initialize files to work on

---
+++ libwyag.py
@@ -0,0 +1,50 @@
+#!/user/bin/env python3
+
+import argparse
+import collections
+import configparser
+from datetime import datetime
+import grp
+import pwd
+from fnmatch import fnmatch
+import hashlib
+from math import ceil
+import os
+import re
+import sys
+import zlib
+from pprint import pprint
+
+# parser = argparse.ArgumentParser(description="this description")
+# parser.add_argument("-v", "--verbose", help="increase verbosity", action="store_true")
+# args = parser.parse_args()
+# print(pprint(args))
+
+
+def main(argv=sys.argv[1:]):
+	argparser = argparse.ArgumentParser(description= "Content Tracker")
+	argsubparser = argparser.add_subparsers(title= "Command", dest= "command")
+	argsubparser.required = True
+	args = argparser.parse_args(argv)
+	match args.command:
+		case "add"          : cmd_add(args)
+		case "cat-file"     : cmd_cat_file(args)
+		case "check-ignore" : cmd_check_ignore(args)
+		case "checkout"     : cmd_checkout(args)
+		case "commit"       : cmd_commit(args)
+		case "hash-object"  : cmd_hash_object(args)
+		case "init"         : cmd_init(args)
+		case "log"          : cmd_log(args)
+		case "ls-files"     : cmd_ls_files(args)
+		case "ls-tree"      : cmd_ls_tree(args)
+		case "rev-parse"    : cmd_rev_parse(args)
+		case "rm"           : cmd_rm(args)
+		case "show-ref"     : cmd_show_ref(args)
+		case "status"       : cmd_status(args)
+		case "tag"          : cmd_tag(args)
+		case _              : print("Invalid Command.")
+
+main()
+
+
+
